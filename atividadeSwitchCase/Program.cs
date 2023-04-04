b16f7327d1583b92024d8a9c216d0ef8570f5352
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-04-04 09:16:43 -0300

    Realizado atividadeSwitchCase e projetoCalculadora

---
+++ atividadeSwitchCase/Program.cs
@@ -0,0 +1,67 @@
+﻿// Crie um menu de opções de bebidas e peça para o usuário escolher uma das opções.
+// A depender da opção, exiba uma mensagem se gostaria de acrescentar gelo.
+// Se sim, exiba uma mensagem que a bebida em questão será com adicional de gelo, caso contrário, a bebida é sem gelo adicional.
+string gelo, bebida="";
+int escolha = 0; 
+
+Console.WriteLine(@$"
+
+---------------------------------
+|                               |
+|                               |
+|    Programa de escolha        |
+|        de Bebida.             |
+|                               |
+|                               |
+|                               |
+---------------------------------
+
+");
+
+Console.WriteLine(@$"
+Escolha uma das bebidas abaixo digitando apenas o numero ferente a bebida: 
+
+1 - Água.
+2 - Água com gás.
+3 - Refigerante.
+4 - Cerveja.
+5 - Vinho.
+");
+
+escolha = int.Parse(Console.ReadLine());
+
+switch (escolha)
+{
+    case (1):
+        Console.WriteLine("\nA opcáo escolhida foi Água.");
+        bebida = "água";
+        break;
+    case (2):
+        Console.WriteLine("\nA opcáo escolhida foi Agua com Água com gás.");
+        bebida = "água com gás";
+        break;
+    case (3):
+        Console.WriteLine("\nA opcáo escolhida foi Refigerante.");
+        bebida = "refigerante";
+        break;
+    case (4):
+        Console.WriteLine("\nA opcáo escolhida foi Cerveja.");
+        bebida = "cerveja";
+        break;
+    case (5):
+        Console.WriteLine("\nA opcáo escolhida foi Vinho.");
+        bebida = "vinho";
+        break;
+    default:
+        Console.WriteLine("\nOpção escolhida é invalida. ");
+        break;
+}
+
+Console.WriteLine($"\nDeseja acresentar gelo em sua {bebida}: sim ou nao");
+    gelo = Console.ReadLine().ToLower();
+
+    if(gelo == "sim"){
+        Console.WriteLine($"\nAcresentado gelo na(o) {bebida}.");
+    }else{
+        Console.WriteLine($"\n{bebida} sem adicional de gelo.");
+    }
--- projetoCalculadora/Program.cs
+++ projetoCalculadora/Program.cs
@@ -1,7 +1,45 @@
 ﻿//Vamos calcular uma calculadora com switch
 
+Console.WriteLine(@$"
+---------------------------------------------
+|                                           |
+|         Programa Calculadora              |
+|   Informe a operaçao que deseja efetuar:  |
+|     '+' somar.                            |
+|     '-' subtração.                        |
+|     '*' multiplicação.                    |
+|     '/' divisão.                          |
+|                                           |
+---------------------------------------------
+");
+
+
+Console.WriteLine($"Digite a operaçao desejada, somente o simbolo: ");
+char operacao = char.Parse(Console.ReadLine());
+
 Console.WriteLine($"DIgite o primeiro numero: ");
 float num1 = float.Parse(Console.ReadLine());
+Console.WriteLine($"DIgite o segundo numero: ");
+float num2 = float.Parse(Console.ReadLine());
+
+switch (operacao){
+    case '+':
+        Console.WriteLine($"{num1} + {num2} = {num1+num2}" );
+        break;
+    case '-':
+        Console.WriteLine($"{num1} - {num2} = {num1-num2}" );
+        break;
+    case '*':
+        Console.WriteLine($"{num1} * {num2} = {num1*num2}" );
+        break;
+    case '/':
+        Console.WriteLine($"{num1} / {num2} = {num1/num2}" );
+        break; 
+    default:
+        Console.WriteLine("A opção escolhida não é valida.");
+        break;
+}
+
 
 
 //Entrada
