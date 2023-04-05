02d9c1c5111cf13abe8de0e8819d143e425294f6
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-04-05 10:57:18 -0300

    Realizado exercicio Do While

---
+++ atividadeDoWhile/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+
+*/
+
+bool idadeCerta = false;
+
+do{
+    Console.WriteLine($"Qual a idade do péricles ?");
+    int idade = int.Parse(Console.ReadLine());
+
+    if (idade ==53){
+        idadeCerta = true;
+    }
+}while(idadeCerta == false);
