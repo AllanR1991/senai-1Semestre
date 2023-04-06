23c5807095c6631480a8db19a1f3e44f4a433eef
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-04-06 08:34:37 -0300

    Finalizado exercicios de repetição.

---
+++ exerciciosRepeticao/exercicio01/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Sistema que verifica se o numero digitado esta entre 0 e 10 */
+
+
+Console.WriteLine($"Insira com uma nota entre 0 e 10: ");
+float num = float.Parse(Console.ReadLine());
+while(num<0 || num > 10){
+    Console.WriteLine($"numero inserido está errado, insira uma nota entre 0 e 10: ");
+    num = float.Parse(Console.ReadLine());
+}
+
---
+++ exerciciosRepeticao/exercicio02/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Sistema que verifica se senha é igual a nome */
+
+
+Console.WriteLine($"Insira seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine($"Insira uma senha (*Deve ser difente do nome): ");
+string senha = Console.ReadLine();
+
+while(senha==nome){
+    Console.WriteLine($"Senha digitada é invalida, favor insira uma senha: (*Deve ser difente do nome): ");
+    senha = Console.ReadLine();
+}
