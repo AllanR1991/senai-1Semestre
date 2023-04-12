62bd46c2e76db1fcb9fce6de88545815acb42a70
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-04-12 10:49:23 -0300

    Realizado programa carregando

---
+++ carregando/Program.cs
@@ -0,0 +1,34 @@
+﻿static string PerguntaString(string pergunta){
+    Console.WriteLine(pergunta);
+    return Console.ReadLine();
+}
+
+static int PerguntaInt(string pergunta){
+    Console.WriteLine(pergunta);
+    return int.Parse(Console.ReadLine());
+}
+
+static float PerguntaFloat(string pergunta){
+    Console.WriteLine(pergunta);
+    return float.Parse(Console.ReadLine());
+}
+
+static void ExibeMensagemPulandoLinha(string texto){
+    Console.WriteLine(texto);
+}
+
+static void ExibeMensagem(string texto){
+    Console.Write(texto);
+}
+
+/* Carregando............. */
+
+static void BarraCarregamento(string texto, int quantidadePontinhos,int tempo){
+    ExibeMensagem(texto);
+    for (int i = 0; i <= quantidadePontinhos; i++){
+        ExibeMensagem(".");
+        Thread.Sleep(tempo);
+    }
+}
+
+BarraCarregamento("Carregando",10,1000);
---
+++ funcao/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Um método é um bloco de código que só é executado quando é chamado.
+
+Você pode passar dados, conhecidos como parâmetros, para um método.
+
+Os métodos são usados ​​para executar determinadas ações e também são conhecidos como funções .
+
+Por que usar métodos? Para reutilizar o código: defina o código uma vez e use-o várias vezes.*/
+
+/*
+Paschal Case
+camelCase
+snake_case
+*/
+
+static float Somar(float n1, float n2){
+    return n1+n2;
+}
+
+static float Subtracao(float n1, float n2){
+    return n1-n2;
+}
+
+static float Multiplicacao(float n1, float n2){
+    return n1*n2;
+}
+
+static float Divisao(float n1, float n2){
+    return n1/n2;
+}
+
+static string PerguntaString(string pergunta){
+    Console.WriteLine(pergunta);
+    return Console.ReadLine();
+}
+
+static int PerguntaInt(string pergunta){
+    Console.WriteLine(pergunta);
+    return int.Parse(Console.ReadLine());
+}
+
+static float PerguntaFloat(string pergunta){
+    Console.WriteLine(pergunta);
+    return float.Parse(Console.ReadLine());
+}
+
+static void ExibeMensagem(string texto){
+    Console.WriteLine(texto);
+}
+
+float num1, num2;
+
+num1 = PerguntaFloat($"Digite o primeiro número: ");
+num2 = PerguntaFloat($"Digite o segundo número: ");
+
+ExibeMensagem($"A soma do {num1} + {num2} = {Somar(num1,num2)}");
+ExibeMensagem($"A subtração do {num1} - {num2} = {Subtracao(num1,num2)}");
+ExibeMensagem($"A multiplicação do {num1} * {num2} = {Multiplicacao(num1,num2)}");
+ExibeMensagem($"A divisão do {num1} / {num2} = {Divisao(num1,num2)}");
