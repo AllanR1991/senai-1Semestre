e91afdba54c198f15c16647e92c5f18365147188
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-04-12 08:42:13 -0300

    finalizado exercicios de array

--- arrayExercicio/exercicio02/Program.cs
+++ arrayExercicio/exercicio02/Program.cs
@@ -8,16 +8,14 @@ for(int index = 0; index < nomes.Length;index++ ){
     nomes[index] = Console.ReadLine();
     Console.WriteLine($"Srº(ª) {nomes[index]} o digite sua idade : ");
     idades[index] = int.Parse(Console.ReadLine());
-
-    
 }
 
 foreach(string nome in nomes){
     Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine($"\n{posicao+1}) nome: {nome}");
+    Console.WriteLine($"\n {posicao+1}) nome: {nome}");
     Console.ResetColor();
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"idade: {idades[posicao]}");
+    Console.WriteLine($"    idade: {idades[posicao]}");
     posicao++;
     Console.ResetColor();
 }
